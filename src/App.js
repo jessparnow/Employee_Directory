4cf1fa8f589c6a584cebb4600c5faba68611fa1b
jessparnow <71057611+jessparnow@users.noreply.github.com>
2021-03-01 21:53:05 -0600

    trying to filter

--- src/App.js
+++ src/App.js
@@ -8,48 +8,75 @@ import Button from "./components/Button/Button";
 import IdButton from "./components/IdButton/IdButton";
 class App extends Component {
   state = {
-      employees: employees
-    }
- 
+    employees: employees,
+    search: "",
+  };
 
   sortBy = () => {
-     let sortedEmployee = this.state.employees.sort((a, b) => (a.name > b.name) ? 1 : -1);
-     //target the name to sort
-     this.setState({sortedEmployee})
-  }
-  sortById = () => {
-    let sortedEmployeeId = this.state.employees.sort((a, b) => (a.id > b.id) ? 1 : -1);
+    let sortedEmployee = this.state.employees.sort((a, b) =>
+      a.name > b.name ? 1 : -1
+    );
     //target the name to sort
-    this.setState({sortedEmployeeId})
- }
- 
-  //   handleToggle = () => {
-  //     if (this.state.sorted) {
-  //       this.props.sort("asc")
-  //     } else {
-  //       this.props.sort("desc")
-  //     }
-  //     this.setState({sorted: !this.state.sorted})
+    this.setState({ sortedEmployee });
+  };
+  sortById = () => {
+    let sortedEmployeeId = this.state.employees.sort((a, b) =>
+      a.id > b.id ? 1 : -1
+    );
+    //target the name to sort
+    this.setState({ sortedEmployeeId });
+  };
+  //  handleChange = (e) => {
+  //    let search = this.state.employees.filter(employees.name);
+  // this.setState({search})
+  //  };
+  // componentDidMount() {
+  //   this.searchEmployees = () => {
+  //     fetch(employees)
+  //       .then((res) => res.json())
+  //       .then((employees) => this.setState({ employees }));
+  //   };
+  // }
+
+  // handleInputChange = (event) => {
+  //   this.setState({ search: event.target.value });
+  // };
+
+  // handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   const value = event.target.value;
+  //   const name = event.target.name;
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // };
+  // onChangeHandler(e){
+  //   this.setState({
+  //     input: e.target.value,
+  //   })
   // }
 
   render() {
+    // const tBody = this.state.users
+    //     .filter(d => this.state.input === '' || d.includes(this.state.input))
+    //     .map((d, index) => <li key={index}>{d}</li>);
 
     return (
       <div>
-        <Banner />
+        <Banner
+          searchEmployees={this.searchEmployees}
+          handleFormSubmit={this.handleFormSubmit}
+          handleInputChange={this.handleInputChange}
+        />
 
         <table className="table table-striped table-dark">
           <thead>
-            
-          <IdButton 
-          sortById={this.sortById}/>
-          <Button
-          sortBy={this.sortBy} />
-          <TableHeader
-          // employees = {employeeData[0]}
-          />
-          
-       </thead>
+            <IdButton sortById={this.sortById} />
+            <Button sortBy={this.sortBy} />
+            <TableHeader
+            // employees = {employeeData[0]}
+            />
+          </thead>
           <tbody>
             {this.state.employees.map((employees) => (
               <EmployeeTable
--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,18 +1,19 @@
 import React from "react";
 
-function Banner() {
+function Banner(props) {
   
   return (
     <nav className="navbar navbar-light bg-light">
   <div className="container-fluid">
     <span className="navbar-brand mb-0 h1">Employee Directory</span>
     <form className="d-flex">
-      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-      <button className="btn btn-outline-success" type="submit">Search</button>
+      <input className="form-control me-2" type="search" placeholder="Search by Name Only" aria-label="Search" />
+      <button className="btn btn-outline-success" type="submit" onClick={(e) => {
+        props.handleInputChange(e)
+      }}>Search</button>
     </form>
   </div>
 </nav>
-
 
   );
 }
